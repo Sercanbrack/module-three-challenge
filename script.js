becf676f14491f8b1616ba502675520e4161cc5e
Samuel Ercanbrack <sam.ercanbrack@outlook.com>
2022-05-01 15:58:04 -0600

    Set up validation for the password length.

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var generatedPassword 
+function generatePassword() {
+
+  //Prompt with the criteria
+  var userLength = prompt("Welcome to the password generator! Please input your desired length of password. NOTE: MUST CONTAIN BETWEEN 8-128 CHARACTERS.")
+  if (userLength > 128) {
+    var userLength = alert("ERROR: Password cannot contain more than 128 characters. Please try again.")
+    generatePassword()
+  } else if (userLength < 8) {
+    var userLength = alert("ERROR: Password cannot contain less than 8 characters. Please try again.")
+    generatePassword()
+  } else {
+    console.log(userLength)
+  }
+  
+
+
+  return "Generated Password"
+}
 
 // Write password to the #password input
 function writePassword() {
